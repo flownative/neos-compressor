6c03cdca09179c2d700fb3af66c93b4b9d7b54be
Karsten Dambekalns <karsten@dambekalns.de>
2022-01-24 10:55:41 +0100

    Update wyrihaximus/html-compress to 4.0

--- Classes/CompressionHelper.php
+++ Classes/CompressionHelper.php
@@ -1,42 +1,46 @@
 <?php
+declare(strict_types=1);
+
 namespace Flownative\Neos\Compressor;
 
 use Neos\Eel\ProtectedContextAwareInterface;
+use voku\helper\HtmlMin;
 use WyriHaximus\HtmlCompress\Factory;
-use WyriHaximus\HtmlCompress\Parser;
+use WyriHaximus\HtmlCompress\HtmlCompressorInterface;
 
 class CompressionHelper implements ProtectedContextAwareInterface
 {
     /**
-     * @var Parser
+     * @var HtmlCompressorInterface
      */
-    protected $parser;
+    protected $compressor;
 
     /**
      * CompressionHelper constructor.
      */
     public function __construct()
     {
-        $this->parser = Factory::construct();
+        $htmlMin = new HtmlMin();
+        // TODO this makes it not break the cache markers, but compresses less (or not at all?)
+        $htmlMin->doOptimizeViaHtmlDomParser(false);
+        $this->compressor = Factory::constructSmallest()->withHtmlMin($htmlMin);
     }
 
     /**
      * Run the value through the compressor.
      *
      * @param string $content
-     *
      * @return string
      */
-    public function compress($content)
+    public function compress(string $content): string
     {
-        return $this->parser->compress($content);
+        return $this->compressor->compress($content);
     }
 
     /**
      * All methods are considered safe, i.e. can be executed from within Eel
      *
      * @param string $methodName
-     *
      * @return boolean
      */
     public function allowsCallOfMethod($methodName)
